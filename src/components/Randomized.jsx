cf0c4af073430388446396647ad507a2780d008f
JoelSRJE <JoelSRJE@hotmail.com>
2023-09-25 18:06:13 +0200

    render random products on home

---
+++ src/components/Randomized.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "../styles/Home.css";
+
+const Randomized = ({ product }) => {
+  return (
+    <div className="product-card">
+      <div className="product-logo">
+        <img src={product.imgUrl} />
+      </div>
+      <hr />
+      <div className="product-title-container">
+        <span>{product.productName}</span>
+      </div>
+      <div className="product-sub-container">
+        <span className="product-price">${product.price}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Randomized;
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+import { React, useState } from "react";
 import { ProductList } from "../utils/products";
+import Randomized from "../components/Randomized";
 import SliderHome from "../slider/Slider";
 import "../styles/Home.css";
+
 const getRandomProducts = () => {
-  const randomProds = ProductList.sort();
+  const randomProds = ProductList.toSorted(() => Math.random() - Math.random());
+  return randomProds.slice(0, 8);
 };
 
 const Home = () => {
+  const [randomProducts, setRandomProducts] = useState(() =>
+    getRandomProducts(ProductList)
+  );
+
   return (
     <>
       <SliderHome />
+      <h1 className="section-title">New Arrivals</h1>
+      <hr className="breakline" />
+      <section id="home-products">
+        {randomProducts.map((product) => (
+          <Randomized product={product} />
+        ))}
+      </section>
+      <hr className="breakline" />
     </>
   );
 };
