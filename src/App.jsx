31234593aea29bb56c5b93b91ae9220f115bc91e
JoelSRJE <JoelSRJE@hotmail.com>
2023-09-25 08:44:47 +0200

    First commit, some setup

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import "./App.css";
+import about from "./views/About";
+import contact from "./views/Contact";
+import shop from "./views/Shop";
+import home from "./views/Home";
+
+/* Npm som används
+
+vite
+react-router-dom
+react-icons
+react-slick
+react-carousel
+
+*/
+
+function App() {
+  return (
+    <>
+      <div>Test text!</div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/utils/Products.jsx
@@ -0,0 +1,518 @@
+import cap1front from "../images/cap1front.png";
+import cap1back from "../images/cap1back.jpg";
+import cap2front from "../images/cap2front.webp";
+import cap2back from "../images/cap2back.webp";
+import cap3front from "../images/cap3front.png";
+import cap3back from "../images/cap3back.jpg";
+import cap4front from "../images//cap4front.webp";
+import cap4back from "../images/cap4back.webp";
+
+import hat1front from "../images/hat1front.png";
+import hat1back from "../images/hat1back.jpg";
+import hat2front from "../images/hat2front.webp";
+import hat2back from "../images/hat2back.webp";
+import hat3front from "../images/hat3front.webp";
+import hat3back from "../images/hat3back.webp";
+import hat4front from "../images/hat4front.webp";
+import hat4back from "../images/hat4back.webp";
+
+import hoodie1front from "../images/hoodie1front.webp";
+import hoodie1back from "../images/hoodie1back.webp";
+import hoodie2front from "../images/hoodie2front.webp";
+import hoodie2back from "../images/hoodie2back.webp";
+import hoodie3front from "../images/hoodie3front.png";
+import hoodie3back from "../images/hoodie3back.jpg";
+import hoodie4front from "../images/hoodie4front.webp";
+import hoodie4back from "../images/hoodie4back.webp";
+
+import sweat1front from "../images/sweat1front.png";
+import sweat1back from "../images/sweat1back.jpg";
+import sweat2front from "../images/sweat2front.webp";
+import sweat2back from "../images/sweat2back.webp";
+import sweat3front from "../images/sweat3front.webp";
+import sweat3back from "../images/sweat3back.webp";
+import sweat4front from "../images/sweat4front.webp";
+import sweat4back from "../images/sweat4back.webp";
+
+/* Pants */
+import pants1front from "../images/pants1front.webp";
+import pants1back from "../images/pants1back.webp";
+import pants2front from "../images/pants2front.webp";
+import pants2back from "../images/pants2back.webp";
+import pants3front from "../images/pants3front.webp";
+import pants3back from "../images/pants3back.webp";
+import pants4front from "../images/pants4front.png";
+import pants4back from "../images/pants4back.jpg";
+import pants5front from "../images/pants5front.webp";
+import pants5back from "../images/pants5back.webp";
+import pants6front from "../images/pants6front.webp";
+import pants6back from "../images/pants6back.webp";
+import pants7front from "../images/pants7front.webp";
+import pants7back from "../images/pants7back.webp";
+import pants8front from "../images/pants8front.webp";
+import pants8back from "../images/pants8back.webp";
+
+import shirt1front from "../images/shirt1front.webp";
+import shirt1back from "../images/shirt1back.webp";
+import shirt2front from "../images/shirt2front.webp";
+import shirt2back from "../images/shirt2back.webp";
+import shirt3front from "../images/shirt3front.webp";
+import shirt3back from "../images/shirt3back.webp";
+import shirt4front from "../images/shirt4front.webp";
+import shirt4back from "../images/shirt4back.webp";
+import shirt5front from "../images/shirt5front.webp";
+import shirt5back from "../images/shirt5back.webp";
+import shirt6front from "../images/shirt6front.webp";
+import shirt6back from "../images/shirt6back.webp";
+import shirt7front from "../images/shirt7front.webp";
+import shirt7back from "../images/shirt7back.webp";
+import shirt8front from "../images/shirt8front.webp";
+import shirt8back from "../images/shirt8back.webp";
+
+import shoe1front from "../images/shoe1front.png";
+import shoe1back from "../images/shoe1back.jpg";
+import shoe2front from "../images/shoe2front.webp";
+import shoe2back from "../images/shoe2back.webp";
+import shoe3front from "../images/shoe3front.webp";
+import shoe3back from "../images/shoe3back.webp";
+import shoe4front from "../images/shoe4front.webp";
+import shoe4back from "../images/shoe4back.webp";
+import shoe5front from "../images/shoe5front.png";
+import shoe5back from "../images/shoe5back.jpg";
+import shoe6front from "../images/shoe6front.webp";
+import shoe6back from "../images/shoe6back.webp";
+import shoe7front from "../images/shoe7front.png";
+import shoe7back from "../images/shoe7back.jpg";
+import shoe8front from "../images/shoe8front.png";
+import shoe8back from "../images/shoe8back.jpg";
+
+/* Produkterna */
+
+export const ProductList = [
+  /* Skorna */
+  {
+    id: "01",
+    productName: "Sneaky Steve",
+    imgUrl: shoe1front,
+    category: "Shoes",
+    subCategory: "Sneakers",
+    price: 120,
+    description:
+      "Passform: Normal  Överdel: 100% läder Övre foder: mikrofiber, textil   Innersula: 100% mikrofiber  Yttersula: 100% gummi	",
+  },
+
+  {
+    id: "02",
+    productName: "Derek",
+    imgUrl: shoe2front,
+    category: "Shoes",
+    subCategory: "Sneakers",
+    price: 140,
+    description:
+      "	Överdel: 100% cow mocka Övre foder: textil: 100% grs återvunnen polyester Innersula: getskinn läder: kromfri garvad Yttersula: tr    Klackhöjd: 4 cm",
+  },
+
+  {
+    id: "03",
+    productName: "Polo Ralph Lauren",
+    imgUrl: shoe3front,
+    category: "Shoes",
+    subCategory: "Sneakers",
+    price: 120,
+    description: "	Material: 100% koskinn	",
+  },
+
+  {
+    id: "04",
+    productName: "Garment Project",
+    imgUrl: shoe4front,
+    category: "Shoes",
+    subCategory: "Sneakers",
+    price: 250,
+    description:
+      "	Överdel: 100% textil, läder Övre foder: 100% textil 	Innersula: 100% polyuretan skum Yttersula: 100% gummi    ",
+  },
+
+  {
+    id: "05",
+    productName: "Björn Borg",
+    imgUrl: shoe5front,
+    category: "Shoes",
+    subCategory: "Sneakers",
+    price: 140,
+    description:
+      "	Foder: 60% polyester, 30% Yttermaterial: 54% koskinn, 46% cow mocka  Överdel: läder, hairy mocka Övre foder: 100% textil Innersula: 60% polyester, 30% Yttersula: 100% tr ",
+  },
+
+  {
+    id: "06",
+    productName: "Mango",
+    imgUrl: shoe6front,
+    category: "Shoes",
+    subCategory: "Sneakers",
+    price: 85,
+    description: "	Material: 100% bovine läder ",
+  },
+
+  {
+    id: "07",
+    productName: "Mango",
+    imgUrl: shoe7front,
+    category: "Shoes",
+    subCategory: "Sneakers",
+    price: 170,
+    description:
+      "	Passform: Normal    Överdel: 100% läder Övre foder: 00% läder    Innersula: 100% läder  Yttersula: 100% gummi  Klackhöjd: 2.5 cm",
+  },
+  {
+    id: "08",
+    productName: "Rieker",
+    imgUrl: shoe8front,
+    category: "Shoes",
+    subCategory: "Sneakers",
+    price: 75,
+    description:
+      "Passform: Normal Överdel: 100% läder Innersula: 100% textil  Yttersula: 100% termoplastisk elastomer Klackhöjd: 2 cm",
+  },
+
+  /* Hoodies & Stickade tröjor */
+
+  {
+    id: "09",
+    productName: "EA7",
+    imgUrl: hoodie1front,
+    category: "Shirts",
+    subCategory: "Hoodie",
+    price: 150,
+    description: "Material: 84% bomull, 16% polyester",
+  },
+
+  {
+    id: "10",
+    productName: "Polo Ralph Lauren",
+    imgUrl: hoodie2front,
+    category: "Shirts",
+    subCategory: "Hoodie",
+    price: 220,
+    description: "Material: 70% bomull 30% polyester",
+  },
+
+  {
+    id: "11",
+    productName: "Fred Perry",
+    imgUrl: hoodie3front,
+    category: "Shirts",
+    subCategory: "Hoodie",
+    price: 150,
+    description: "Material: 70% bomull 30% polyester",
+  },
+
+  {
+    id: "12",
+    productName: "Makia",
+    imgUrl: hoodie4front,
+    category: "Shirts",
+    subCategory: "Hoodie",
+    price: 110,
+    description: "Material: 100% ekologisk bomull",
+  },
+
+  {
+    id: "13",
+    productName: "Tommy Hilfiger",
+    imgUrl: sweat1front,
+    category: "Shirts",
+    subCategory: "Sweater",
+    price: 160,
+    description: "Material: 64% tco, 36% återvunnen polyester",
+  },
+
+  {
+    id: "14",
+    productName: "Makia",
+    imgUrl: sweat2front,
+    category: "Shirts",
+    subCategory: "Sweater",
+    price: 100,
+    description: "Material: 100% ekologisk bomull",
+  },
+
+  {
+    id: "15",
+    productName: "Polo Ralph Lauren",
+    imgUrl: sweat3front,
+    category: "Shirts",
+    subCategory: "Sweater",
+    price: 100,
+    description: "Material: 60% bomull, 40% polyester",
+  },
+
+  {
+    id: "16",
+    productName: "Fred Perry",
+    imgUrl: sweat4front,
+    category: "Shirts",
+    subCategory: "Sweater",
+    price: 150,
+    description: "Material: 60% polyester, 34% bomull",
+  },
+
+  /* Casual & Joggingbyxor */
+  {
+    id: "17",
+    productName: "Lacoste",
+    imgUrl: pants1front,
+    category: "Pants",
+    subCategory: "Joggers",
+    price: 165,
+    description: "Material: 51% polyester, 49% bomull",
+  },
+
+  {
+    id: "18",
+    productName: "Tommy Hilfiger",
+    imgUrl: pants2front,
+    category: "Pants",
+    subCategory: "Joggers",
+    price: 70,
+    description: "Material: 100% tco",
+  },
+
+  {
+    id: "19",
+    productName: "Gant",
+    imgUrl: pants3front,
+    category: "Pants",
+    subCategory: "Joggers",
+    price: 160,
+    description:
+      "Material: 70% polyester (återvunnen polyester), 28% bomull, 2% polyuretan",
+  },
+
+  {
+    id: "20",
+    productName: "EA7",
+    imgUrl: pants4front,
+    category: "Pants",
+    subCategory: "Joggers",
+    price: 130,
+    description: "Material: 84% bomull, 16% polyester",
+  },
+
+  {
+    id: "21",
+    productName: "Les Deux",
+    imgUrl: pants5front,
+    category: "Pants",
+    subCategory: "Casual",
+    price: 140,
+    description:
+      "Material: 58% polyester, 30% återvunnen, polyester, 8% viskos, 4% spandex",
+  },
+
+  {
+    id: "22",
+    productName: "Polo Ralph Lauren",
+    imgUrl: pants6front,
+    category: "Pants",
+    subCategory: "Casual",
+    price: 190,
+    description: "Material: 100% bomull",
+  },
+
+  {
+    id: "23",
+    productName: "Makia",
+    imgUrl: pants7front,
+    category: "Pants",
+    subCategory: "Casual",
+    price: 100,
+    description: "Material: 64% bomull, 31% polyester, 5% elastan",
+  },
+
+  {
+    id: "24",
+    productName: "Gabba",
+    imgUrl: pants8front,
+    category: "Pants",
+    subCategory: "Casual",
+    price: 130,
+    description: "Material: 62% polyester, 33% viskos, 5% elastan",
+  },
+
+  /* T-shirts */
+  {
+    id: "25",
+    productName: "BOSS",
+    imgUrl: shirt1front,
+    category: "Shirts",
+    subCategory: "T-shirt",
+    price: 95,
+    description: "Material: 95% bomull 5% elastan",
+  },
+
+  {
+    id: "26",
+    productName: "Fred Perry",
+    imgUrl: shirt2front,
+    category: "Shirts",
+    subCategory: "T-shirt",
+    price: 80,
+    description: "Material: 100% bomull",
+  },
+
+  {
+    id: "27",
+    productName: "Polo Ralph Lauren",
+    imgUrl: shirt3front,
+    category: "Shirts",
+    subCategory: "T-shirt",
+    price: 110,
+    description: "Material: 100% bomull",
+  },
+
+  {
+    id: "28",
+    productName: "Tommy Hilfiger",
+    imgUrl: shirt4front,
+    category: "Shirts",
+    subCategory: "T-shirt",
+    price: 80,
+    description: "Material: 100% tco",
+  },
+
+  {
+    id: "29",
+    productName: "Belstaff",
+    imgUrl: shirt5front,
+    category: "Shirts",
+    subCategory: "T-shirt",
+    price: 85,
+    description: "Material: 100% bomull",
+  },
+
+  {
+    id: "30",
+    productName: "Filippa K",
+    imgUrl: shirt6front,
+    category: "Shirts",
+    subCategory: "T-shirt",
+    price: 60,
+    description: "Material: 100% bomull",
+  },
+
+  {
+    id: "31",
+    productName: "EA7",
+    imgUrl: shirt7front,
+    category: "Shirts",
+    subCategory: "T-shirt",
+    price: 90,
+    description: "Material: 100% bomull",
+  },
+
+  {
+    id: "32",
+    productName: "Polo Ralph Lauren",
+    imgUrl: shirt8front,
+    category: "Shirts",
+    subCategory: "T-shirt",
+    price: 110,
+    description: "Material: 100% bomull",
+  },
+
+  /* Keps & Mössor */
+
+  {
+    id: "33",
+    productName: "The North Face",
+    imgUrl: cap1front,
+    category: "Hats",
+    subCategory: "Cap",
+    price: 35,
+    description: "Material: 100% polyester",
+  },
+
+  {
+    id: "34",
+    productName: "Adidas",
+    imgUrl: cap2front,
+    category: "Hats",
+    subCategory: "Cap",
+    price: 25,
+    description: "Material: 86% återvunnen polyester, 14% elastan",
+  },
+
+  {
+    id: "35",
+    productName: "Boss",
+    imgUrl: cap3front,
+    category: "Hats",
+    subCategory: "Cap",
+    price: 40,
+    description: "Material: 100% bomull",
+  },
+
+  {
+    id: "36",
+    productName: "Puma",
+    imgUrl: cap4front,
+    category: "Hats",
+    subCategory: "Hat",
+    price: 35,
+    description: "Material: 100% bomull, inner: 100% polyester",
+  },
+
+  {
+    id: "37",
+    productName: "The North Face",
+    imgUrl: hat1front,
+    category: "Hats",
+    subCategory: "Hat",
+    price: 30,
+    description: "Material: 100% återvunnen polyester",
+  },
+
+  {
+    id: "38",
+    productName: "Halo",
+    imgUrl: hat2front,
+    category: "Hats",
+    subCategory: "Hat",
+    price: 60,
+    description: "Material: 70% ull, 30% akryl",
+  },
+
+  {
+    id: "39",
+    productName: "New Era",
+    imgUrl: hat3front,
+    category: "Hats",
+    subCategory: "Hat",
+    price: 40,
+    description: "Material: 100% Acrylic",
+  },
+
+  {
+    id: "40",
+    productName: "Helly Hansen",
+    imgUrl: hat4front,
+    category: "Hats",
+    subCategory: "Hat",
+    price: 35,
+    description: "Yttermaterial: 100% akryl",
+  },
+];
+
+/*
+
+  {
+    id: "",
+    productName: "",
+    imgUrl: ,
+    category: "",
+    price: ,
+    description: "",
+  },
+
+*/
