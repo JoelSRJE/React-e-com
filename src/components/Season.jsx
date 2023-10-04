a4bb3c607b96adb3ab64cc82421f0b0b0ff0b341
JoelSRJE <JoelSRJE@hotmail.com>
2023-10-04 20:56:55 +0200

    updated some css, added season banner

---
+++ src/components/Season.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Autumn from "../images/Autumn.jpg";
+
+const Season = () => {
+  return (
+    <div className="season-wrapper">
+      <div className="season-container">
+        <img src={Autumn} />
+      </div>
+    </div>
+  );
+};
+
+export default Season;
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,6 +3,9 @@ import { ProductList } from "../utils/Products";
 import Randomized from "../components/Randomized";
 import SliderHome from "../slider/Slider";
 import "../styles/Home.css";
+import { CategoryData } from "../utils/CategorySelectData";
+import Categories from "../components/Categories";
+import Season from "../components/Season";
 
 const getRandomProducts = () => {
   const randomProds = ProductList.toSorted(() => Math.random() - Math.random());
@@ -17,14 +20,26 @@ const Home = () => {
   return (
     <>
       <SliderHome />
-      <h1 className="section-title">New Arrivals</h1>
+      <h1 className="section-title">Categories</h1>
       <hr className="breakline" />
-      <section id="home-products">
-        {randomProducts.map((product, idx) => (
-          <Randomized product={product} key={idx} />
+      <div className="category-select-container">
+        {CategoryData.map((categoryData, idx) => (
+          <Categories categoryData={categoryData} key={idx} />
         ))}
-      </section>
-      <hr className="breakline" />
+      </div>
+      <Season />
+      <div id="home-products-wrapper">
+        <h1 className="section-title">New Arrivals</h1>
+        <hr className="breakline" />
+        <section id="home-products">
+          <div id="products">
+            {randomProducts.map((product, idx) => (
+              <Randomized product={product} key={idx} />
+            ))}
+          </div>
+        </section>
+        <hr className="breakline" />
+      </div>
     </>
   );
 };
